0d8b0039f6a1693d7fb4727261eb28765293ab30
joevb <joevanbo@protonmail.com>
2017-11-22 22:02:38 +1300

    can save coords to store god bless rich churcher

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Provider from 'react-redux'
+import { Provider } from 'react-redux'
 
 import App from './components/App'
-import store from './store'
+import { store } from './store'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
