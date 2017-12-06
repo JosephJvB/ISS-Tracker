e0a4fdde2255fac420f9cdf813618ee8f30d22c4
JosephJvB <jo-van-bo@hotmail.com>
2017-12-06 23:10:12 +1300

    one passing test

--- tests/__tests__/actions_test.js
+++ tests/__tests__/actions_test.js
@@ -3,5 +3,5 @@ import { addCoords, ADD_COORDS } from '../../client/actions/coords'
 test('addCoords has correct shape', () => {
   const action = addCoords(1, 2)
   expect(action.type).toBe(ADD_COORDS)
-  expect(action.coords).toBe({ lat: 1, lng: 2 })
+  expect(action.coords).toEqual({lat: 1, lng: 2})
 })
