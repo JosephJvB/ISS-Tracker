0033beb1aa92d9795ba03fa06dd5841e8018e9aa
JosephJvB <jo-van-bo@hotmail.com>
2017-12-08 16:05:44 +1300

    pic click test improved

--- tests/nightmare/D_pic_test.js
+++ tests/nightmare/D_pic_test.js
@@ -1,21 +1,12 @@
 Feature('pic click tests')
 
-Scenario('fish or wiki', function * (I) {
+Scenario('see wikipedia or fish info', function * (I) {
   let src = yield I.grabAttributeFrom('.pic', 'src')
+  I.click('.pic')
   if (src.includes('dopefish')) {
     I.seeElement('#fish')
     I.see('Location not specified')
   } else {
-    I.click('#wiki')
     I.seeInCurrentUrl('wikipedia')
   }
 })
-
-Scenario('bigtest', function * (I) {
-  let err = yield I.grabTextFrom('#err')
-  let loc = yield I.grabTextFrom('#loc')
-  if (loc) I.seeElement('#wiki')
-  if (err) I.seeElement('#fish')
-})
-
-// these tests are pretty goofy sure they overlap
