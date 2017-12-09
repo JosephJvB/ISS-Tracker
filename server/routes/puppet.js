3111903c43f939a32ce37720ffc9b51a922a22b1
JosephJvB <jo-van-bo@hotmail.com>
2017-12-10 10:48:01 +1300

    scrape test file works

--- server/routes/puppet.js
+++ server/routes/puppet.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer')
 
-async function scrapePic (city) {
+const bookSrc = 'upload.wikimedia.org/wikipedia/en/thumb/9/99/Question_book-new.svg/50px-Question_book-new.svg.png'
+
+async function scrapePic (city, test) {
   if (city === 'test') return 'great job'
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
@@ -8,15 +10,16 @@ async function scrapePic (city) {
 
   const pic = await page.evaluate(() => {
     let sauce = document.body.querySelector('a.image').innerHTML.split('src="//')[1].split('" ')[0]
-    // if(sauce === 'book pic url') return second('a.image')
+    // if(sauce === 'bookSrc') return second('a.image')
     return sauce
   })
 
   browser.close()
-  // console.log('pic: ', pic)
+  if (test) return console.log('pic: ', pic)
   return pic
 }
 
 module.exports = {scrapePic}
 
 // stop returning the book pic! started some pseudocode
+// Tripoli is a case to test
